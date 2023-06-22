326ac9e64557f4164c75d3a294bc2ea367387896
Joa <Joa@JOA-ESCRITORIO>
2023-06-22 14:54:19 -0300

    Agregar archivos de proyecto.

---
+++ Caballo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller
+{
+    internal class Caballo
+    {
+        class Caracteristicas
+        {
+            int velocidad;  //entre 50-65(3), 65-85(4), 85-105(5)
+            int energEsprint; //entre 50-65(3), 65-85(4), 85-105(5)
+            int aceleracion;  //entre 50-65(3), 65-85(4), 85-105(5)
+            int agilidad;   //entre 50-65(3), 65-85(4), 85-105(5)
+            int salto;     //entre 50-65(3), 65-85(4), 85-105(5)
+            int distancia; //entre 800 - 3200
+            string? pista; //MUY BLANDA/BLANDA/MEDIA/FIRMA/DURA/MUY DURA
+            int estrellas; //entre 3 y 5
+
+        }
+
+        class Datos
+        {
+            int edad;
+            string? nombre;
+            string? raza;
+            string? pelaje;
+            DateTime fechaDeNacimiento;
+
+        }
+
+    }
+}
